79e427d66404059c516d371b821dc65e41be18ae
Shivam Sharma <ssshivamsharma02@gmail.com>
2023-07-31 12:08:13 +0530

    add multiprocess file

--- mutliprocessing_python.py
+++ mutliprocessing_python.py
@@ -4,21 +4,22 @@ def func_lazy(n):
     time.sleep(n)
     string_out = f" Sleep completed with time {str(n)}"
     return string_out
-time1 = time.time()
+# time1 = time.time()
 time_intervals = [1,2,3,4,5,6]
-for i in time_intervals:
-    print(func_lazy(i))
-time2 = time.time()
-print(f"time taken without multiprocessing{time2 - time1}")
-time1 = time.time()
+# for i in time_intervals:
+#     print(func_lazy(i))
+# time2 = time.time()
+# print(f"time taken without multiprocessing: {time2 - time1}")
+
 # using multiprocessing 
 def main():
-    pool = Pool(1)
+    pool = Pool(6)
     result = pool.imap_unordered(func_lazy, time_intervals)
     for each in result:
         print(each)
-time2 = time.time()
-print(f"time taken with multiprocessing{time2 - time1}")
 
 if __name__ == "__main__":
+    time1 = time.time()
     main()
+    time2 = time.time()
+    print(f"time taken with multiprocessing: {time2 - time1}")
